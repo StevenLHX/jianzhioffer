2a63d9de76654ba649d097a65ce9f1830c3902c6
steven <linghuaxing312@163.com>
2019-02-26 20:01:31 +0800

    数值的整数次方

--- 16_Power/src/main/java/com/program/solution/PowerWithExponent.java
+++ 16_Power/src/main/java/com/program/solution/PowerWithExponent.java
@@ -9,8 +9,10 @@ public class PowerWithExponent {
         if (exponent==1){
             return base;
         }
+        //右移代替除以2,
         double result=powerWithExponent(base,exponent>>1);
         result *= result;
+        //用位于运算符代替了求余运算符（%）来判断一个数是奇数还是偶数
         if ((exponent&1)==1){
             result*=base;
         }
